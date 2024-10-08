10f11013087172960cf7b61cd842f0e53979cfad
Kevin Rasmussen <KRasm57@wgu.edu>
2024-10-08 14:30:46 -0500

    Created an easy version of Hangman with a txt list of nouns

--- Hangman_Easy.py
+++ Hangman_Easy.py
@@ -1,23 +1,42 @@
 import random
-import requests
+import time
+import os
+from colorama import init, Fore, Back, Style
 
-response = requests.get('https://random-word-api.herokuapp.com/word')
-word = response.json()[0]
+init()
 
-# Limit the word length to 5 characters
-if len(word) <= 5:
-    print(word)
-else:
-    # If the word is too long, get a new one
-    while len(word) > 5:
-        response = requests.get("https://random-word-api.herokuapp.com/word")
-        word = response.json()[0]
-    #print(word)
-# List of words to choose from
-#words = ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape']
 
-# Choose a random word from the list
-#random_word = random.choice(words)
+def win_animation():
+    print("\n" + Fore.GREEN + "Congratulations, you won!" + Style.RESET_ALL)
+    time.sleep(1)
+    print(Fore.YELLOW + "Well done!" + Style.RESET_ALL)
+    time.sleep(1)
+    print(Fore.CYAN + "You guessed the word correctly!" + Style.RESET_ALL)
+    time.sleep(1)
+    print(Fore.MAGENTA + "Here's a virtual confetti for you:" + Style.RESET_ALL)
+    time.sleep(1)
+
+
+def rainbow_confetti():
+    colors = [Fore.RED, Fore.YELLOW, Fore.GREEN, Fore.CYAN, Fore.BLUE, Fore.MAGENTA, Style.BRIGHT + Fore.RED,
+              Style.BRIGHT + Fore.YELLOW, Style.BRIGHT + Fore.GREEN, Style.BRIGHT + Fore.CYAN, Style.BRIGHT + Fore.BLUE,
+              Style.BRIGHT + Fore.MAGENTA]
+    for row in range(10):
+        for _ in range(10):
+            print(random.choice(colors) + "*", end=" ")
+        print()
+        time.sleep(0.5)  # Add a 0.5 second delay
+    print(Style.RESET_ALL)
+
+
+def get_random_word(top_100_nouns):
+    with open(top_100_nouns, 'r') as f:
+        words = f.readlines()
+        return random.choice(words).strip()
+
+
+random_word = get_random_word('top_100_nouns.txt')
+print(random_word)
 
 # Initialize the hangman figure
 hangman_figure = [
@@ -80,11 +99,11 @@ hangman_figure = [
 ]
 
 # Initialize the word with underscores
+word = random_word
 word_with_underscores = ['_'] * len(word)
 
 # Initialize the number of incorrect guesses
 incorrect_guesses = 0
-
 
 print(r'''
 __        __   _                            _____     
@@ -98,13 +117,21 @@ __        __   _                            _____
 |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_(_)     
                    |___/                              
 ''')
+
 print()
-print(" " * 300 + hangman_figure[0])
+max_width = 80  # adjust this value to match your terminal width
+hangman_width = max(len(line) for line in hangman_figure[0].split('\n'))
+spaces = (max_width - hangman_width) // 2
+
+print("\n" + hangman_figure[0].center(max_width))
 print()
 
 used_letters = []
+
+
 def add_used_letter(guess):
     used_letters.append(guess.upper())
+
 
 while '_' in word_with_underscores and incorrect_guesses < len(hangman_figure) - 1:
     # Ask the user for a guess
@@ -139,9 +166,10 @@ while '_' in word_with_underscores and incorrect_guesses < len(hangman_figure) -
     print(' '.join(word_with_underscores))
     print(hangman_figure[incorrect_guesses])
 
-
 # Check if the user won or lost
 if '_' not in word_with_underscores:
-    print("Congratulations, you won! The word was " + word.upper()+'!')
+    win_animation()
+    rainbow_confetti()
+    # print("\nCongratulations, you won! The word was " + word.upper()+'!\n')
 else:
-    print("Sorry, you lost. The word was " + word.upper()+'!')
+    print("\nSorry, you lost. The word was " + word.upper() + '!\n')
